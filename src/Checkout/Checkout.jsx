fc5f4a4cc4ca1586e22cb2139cb84d0265d00fc4
marwan <marwan4125881@gmail.com>
2025-02-09 12:36:23 +0200

    fix errors

--- src/Checkout/Checkout.jsx
+++ src/Checkout/Checkout.jsx
@@ -98,7 +98,7 @@ export default function Checkout(){
               </div>: null}
 
                 <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{isLoading? "Loding...":"Checkout Now"}</button>
+                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{isLoading? "Loading...":"Checkout Now"}</button>
 
                 </div>
                 </form>
